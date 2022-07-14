f4bd73911385efdfc29942301c1312921431464c
xDIamEvil <flaviupop2001@gmail.com>
2022-07-14 16:44:21 +0300

    no message

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
+import SecondScreen from "./src/screens/SecondScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const homeScreenFlow = createStackNavigator({
+  Search: SearchScreen,
+  SecondScreen: SecondScreen,
 });
+homeScreenFlow.navigationOptions = {
+  initialRouteName: "Search",
+  title: "Search",
+  headerShown: false,
+  tabBarIcon: <FontAwesome name="search" size={24} color="white" />,
+  tabBarOptions: {
+    style: {
+      backgroundColor: "#141414",
+      borderTopColor: "white",
+      borderTopWidth: 0.5,
+    },
+  },
+  tabBarLabel: () => <Text style={{ color: "white" }}>Search</Text>,
+};
+
+SearchScreen.navigationOptions = {
+  headerShown: false,
+};
+
+SecondScreen.navigationOptions = ({ navigation }) => {
+  const movies = navigation.getParam("movies");
+  return {
+    title: `${movies?.Title}`,
+    headerShown: true,
+    headerTitleStyle: {
+      color: "white",
+      alignSelf: "flex-end",
+    },
+    headerStyle: {
+      backgroundColor: "#141414",
+    },
+    headerLeft: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Search")}>
+        <Ionicons
+          name="arrow-back"
+          size={24}
+          color="white"
+          style={{ marginLeft: 20 }}
+        />
+      </TouchableOpacity>
+    ),
+  };
+};
+
+export default createAppContainer(homeScreenFlow);
